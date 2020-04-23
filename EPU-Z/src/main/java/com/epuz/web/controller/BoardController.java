66ac894f48588b1c3fb352304456c0c354ee7e37
kwon1993 <wdmono@naver.com>
2020-04-23 22:40:16 +0900

    text editor summernote add.. image upload in progress

--- EPU-Z/src/main/java/com/epuz/web/controller/BoardController.java
+++ EPU-Z/src/main/java/com/epuz/web/controller/BoardController.java
@@ -1,5 +1,11 @@
 package com.epuz.web.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,10 +13,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.epuz.web.dto.FreeBoardModifyDTO;
 import com.epuz.web.dto.FreeBoardRegistrationDTO;
 import com.epuz.web.service.BoardService;
+import com.google.gson.JsonObject;
 
 @Controller
 @RequestMapping("/board")
@@ -40,6 +49,33 @@ public class BoardController {
 	public String freeBoardRegistration(FreeBoardRegistrationDTO freeBoardRegistrationDTO, Model model) {
 		boardService.FreeBoardRegistration(freeBoardRegistrationDTO.getTitle(), freeBoardRegistrationDTO.getWriter(), freeBoardRegistrationDTO.getContent());
 		return "redirect:freeBoardList";
+	}
+	
+	@PostMapping(value="/uploadSummernoteImageFile", produces = "application/json")
+	@ResponseBody
+	public JsonObject uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile) {
+		JsonObject jsonObject = new JsonObject();
+		
+		String fileRoot = "C:\\Users\\wdmono\\Desktop\\fileupload";
+		String originalFileName = multipartFile.getOriginalFilename();
+		String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+		
+		String savedFileName = UUID.randomUUID() + extension;
+		
+		File targetFile = new File(fileRoot + savedFileName);
+		
+		try {
+			InputStream fileStream = multipartFile.getInputStream();
+			FileUtils.copyInputStreamToFile(fileStream, targetFile);
+			jsonObject.addProperty("url", "/summernoteImage/"+savedFileName);
+			jsonObject.addProperty("responseCode", "success");
+		}catch(IOException e) {
+			FileUtils.deleteQuietly(targetFile);
+			jsonObject.addProperty("responseCode", "error");
+			e.printStackTrace();
+		}
+		
+		return jsonObject;
 	}
 	
 	@GetMapping("/freeBoardModify")
